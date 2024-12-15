fc18992cd4c029a1e24291d40fe35d0f5985a03e
kawasaki <yomitanmasa3523@gmail.com>
2024-12-15 21:34:20 +0900

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,35 @@
+import { Box, Flex, Text, Link, VStack, HStack } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+import SideBar from '@/components/parts/SideBar.tsx';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Dashboard = ({ children }: LayoutProps) => {
+  return (
+    <Flex h='100vh'>
+      <SideBar />
+
+      {/* メインコンテンツ */}
+      <Flex flex='1' direction='column'>
+        {/* ヘッダー */}
+        <Box bg='teal.500' p={4} color='white' boxShadow='sm'>
+          <HStack justify='space-between'>
+            <Text fontSize='xl' fontWeight='bold'>
+              ダッシュボード
+            </Text>
+            <Text fontSize='sm'>Admin</Text>
+          </HStack>
+        </Box>
+
+        {/* コンテンツ */}
+        <Box p={6} bg='gray.50' flex='1' overflowY='auto'>
+          {children}
+        </Box>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Dashboard
