a94090124f1ee130140b8802cfce6ed31aa1550c
kawasaki <yomitanmasa3523@gmail.com>
2025-04-26 21:19:47 +0900

    change

--- src/app/fish-categories/[id]/page.tsx
+++ src/app/fish-categories/[id]/page.tsx
@@ -1,52 +1,55 @@
 import { Box } from '@chakra-ui/react'
 import FishCategoryUpdate from '@/components/pages/fishCategory/update/index'
+import Layout from '@/components/parts/Layout'
 
 export const revalidate = 60
 
 export const generateStaticParams = async (): Promise<{ id: string }[]> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/admin/fish-categories`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    cache: 'force-cache',
-  })
-  if (!res.ok) {
-    return []
-  }
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/admin/fish-categories`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        cache: 'force-cache',
+    })
+    if (!res.ok) {
+        return []
+    }
 
-  const fishCategories = await res.json()
+    const fishCategories = await res.json()
 
-  return fishCategories.map((category: { id: number }) => ({
-    id: category.id.toString(),
-  }))
+    return fishCategories.map((category: { id: number }) => ({
+        id: category.id.toString(),
+    }))
 }
 
 type FishCategoryPageProps = {
-  params: Promise<{
-    id: string 
-  }>
+    params: Promise<{
+        id: string 
+    }>
 }
 
 const FishCategoryUpdatePage = async ({ params }: FishCategoryPageProps) => {
-  const { id } = await params
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/admin/fish-categories/${id}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+    const { id } = await params
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/admin/fish-categories/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
 
-  if (!res.ok) {
-    return <div>エラーが発生しました。</div>
-  }
+    if (!res.ok) {
+        return <div>エラーが発生しました。</div>
+    }
 
-  const fishCategory = await res.json()
-  return (
-    <Box p={4} bg='white' borderRadius='md' boxShadow='sm'>
-      <FishCategoryUpdate fishCategory={fishCategory} />
-    </Box>
-  )
+    const fishCategory = await res.json()
+    return (
+        <Layout>
+            <Box p={4} bg='white' borderRadius='md' boxShadow='sm'>
+                <FishCategoryUpdate fishCategory={fishCategory} />
+            </Box>
+        </Layout>
+    )
 }
 
 export default FishCategoryUpdatePage
